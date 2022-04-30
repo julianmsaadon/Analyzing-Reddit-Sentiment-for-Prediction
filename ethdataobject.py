e7d436e0233a436f6cf70652beff613895988871
jmsaadon <jmsaadon@gmail.com>
2022-04-30 17:35:15 -0400

    Added to the object

--- ethdataobject.py
+++ ethdataobject.py
@@ -1,5 +1,10 @@
 class ethdata:
 
-    def __init__(self, Time, Closeprice,):
+    def __init__(self, Time, Closeprice, TransactionVolume, EthSpentOverTime, SocialVolumeReddit, SentimentVolumeConsumedReddit, SentimentBalanceReddit, ):
         self.Time = Time
         self.Closeprice = Closeprice
+        self.TransactionVolume = TransactionVolume
+        self.SocialVolumeReddit = SocialVolumeReddit
+        self.EthSpentOverTime = EthSpentOverTime
+        self.SentimentVolumeConsumedReddit = SentimentVolumeConsumedReddit
+        self.SentimentBalanceReddit = SentimentBalanceReddit
